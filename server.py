301e167345034ace86fe781832b5923b06810d10
diegofdxz <diegfefe@myuax.com>
2026-04-19 19:57:44 +0200

    Correción de CSRF

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
+from datetime import timedelta
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
-app.permanent_session_lifetime = 99999999
+
+app.config.update(
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+    SESSION_COOKIE_SECURE=False
+)
+
+app.permanent_session_lifetime = timedelta(days=7)
+
 
 @app.errorhandler(404)
 def not_found(e):
